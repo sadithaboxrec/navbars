6496616f76ee8bd69a0863d90ecfd0a6a4ca9d8a
Saditha Fernando <sadithafernandods2002@gmail.com>
2025-06-25 20:03:23 +0530

    Mobile Responsiveness Adding

--- navbar5-fixed-active/index.js
+++ navbar5-fixed-active/index.js
@@ -10,3 +10,10 @@ document.querySelectorAll('.navbar-menu a').forEach(link => {
 
 
 
+const navbarToggle = document.querySelector('.navbar-toggle');
+const navbarMenu = document.querySelector('.navbar-menu');
+
+navbarToggle.addEventListener('click', () => {
+    navbarToggle.classList.toggle('active');
+    navbarMenu.classList.toggle('active');
+});
